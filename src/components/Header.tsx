57d7f85b385633b4c979ffd48e91067505db76ae
VermiNew <werminew@gmail.com>
2026-02-22 18:22:51 +0100

    style(ui): add icons to navigation links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,16 @@
 import { NavLink } from 'react-router-dom'
+import { FaCross, FaBookBible, FaMusic, FaMagnifyingGlass } from 'react-icons/fa6'
 
 export default function Header() {
   return (
     <header className="header">
       <nav className="nav">
-        <NavLink to="/" className="nav-brand">Słowo Życia</NavLink>
+        <NavLink to="/" className="nav-brand">✝ Słowo Życia</NavLink>
         <ul className="nav-links">
-          <li><NavLink to="/modlitwy">Modlitwy</NavLink></li>
-          <li><NavLink to="/pismo-swiete">Pismo Święte</NavLink></li>
-          <li><NavLink to="/spiewnik">Śpiewnik</NavLink></li>
-          <li><NavLink to="/szukaj">Szukaj</NavLink></li>
+          <li><NavLink to="/modlitwy"><FaCross /> Modlitwy</NavLink></li>
+          <li><NavLink to="/pismo-swiete"><FaBookBible /> Pismo Święte</NavLink></li>
+          <li><NavLink to="/spiewnik"><FaMusic /> Śpiewnik</NavLink></li>
+          <li><NavLink to="/szukaj"><FaMagnifyingGlass /> Szukaj</NavLink></li>
         </ul>
       </nav>
     </header>
